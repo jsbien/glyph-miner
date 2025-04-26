b92117dfd750640d5a8f0ca5c86faee3843a9c94
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-04-26 15:56:35 +0200

    new

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-echo "Starting mysql"
+# Start MySQL
 service mysql start
-
-# Wait for MySQL to start up
 sleep 5
 
-# Create database and user
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS glyphminer;"
-mysql -uroot -e "CREATE USER IF NOT EXISTS 'glyphminer'@'localhost' IDENTIFIED WITH mysql_native_password BY 'glyphminer';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON glyphminer.* TO 'glyphminer'@'localhost';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
-
-echo "Starting nginx"
+# Start nginx
 service nginx start
 
-echo "Starting the python backend"
-/usr/local/bin/uwsgi --http :9090 --chdir /opt --wsgi-file glyph-miner/wsgi.py --callable app --pythonpath /opt/glyph-miner/web --processes 2 --threads 2 --master
-
-
+# Start the uwsgi server manually
+uwsgi --http :9090 --wsgi-file server/server.py --callable app
