49ff5c3c7ea9f74c0bcf6b6682afe1486cd7d59c
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-04-26 17:13:59 +0200

    a next fix

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -7,8 +7,8 @@ sleep 5
 # Start nginx
 service nginx start
 
-# Add server/ and server/web/ folders to PYTHONPATH
+# Set correct PYTHONPATH
 export PYTHONPATH=/opt/glyph-miner/server:/opt/glyph-miner/server/web:$PYTHONPATH
 
-# Start uwsgi server
-uwsgi --http :9090 --wsgi-file server/server.py --callable app
+# Start uwsgi by module, not by file
+uwsgi --http :9090 --module server.server:app
