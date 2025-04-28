734e4cbbede2d19ca6807eb24d942060d8eefa06
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-04-28 16:08:14 +0200

    update

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+set -e
 
 # Start MySQL
+echo "[INFO] Starting MySQL..."
 service mysql start
 sleep 5
 
 # Start nginx
+echo "[INFO] Starting nginx..."
 service nginx start
 
 # Set correct PYTHONPATH
 export PYTHONPATH=/opt/glyph-miner:$PYTHONPATH
 
-# Start uwsgi properly
-# uwsgi --http :9090 --chdir /opt/glyph-miner --module server.server:app
-# uwsgi --http :9090 --chdir /opt/glyph-miner --module server.server:application
-# uwsgi --http :9090 --chdir /opt/glyph-miner --module server.server:application --catch-exceptions --py-autoreload 1 --honour-stdin
-uwsgi --http :9090 --chdir /opt/glyph-miner --module server.server:app --catch-exceptions --py-autoreload 1 --honour-stdin
+# Start uWSGI in socket mode (like original)
+echo "[INFO] Starting uWSGI..."
+uwsgi --socket 127.0.0.1:9090 --chdir /opt/glyph-miner --module server.server:app --catch-exceptions --py-autoreload 1 --honour-stdin
