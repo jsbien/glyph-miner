51324fd32b5b10cb79786e121059dabfa5e90473
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-04-28 14:23:30 +0200

    new version

--- server/server.py
+++ server/server.py
@@ -1,60 +1,51 @@
 # coding: utf-8
-from server.webapp.application import application  # <-- fixed import!
+from server.webapp.application import application
+import json
 
-class server:
-    def GET(self):
-        with open("web/index.html", "r", encoding="utf-8") as f:
-            html = f.read()
-        return html
+# Dummy handlers
+def get_collections():
+    return [
+        '200 OK',
+        [('Content-Type', 'application/json')],
+        [json.dumps([]).encode('utf-8')]
+    ]
 
-    def __call__(self):
-        return self.GET()
+def get_images():
+    return [
+        '200 OK',
+        [('Content-Type', 'application/json')],
+        [json.dumps([]).encode('utf-8')]
+    ]
 
-class loader:
-    def POST(self):
-        return "Loader not implemented yet."
+def get_glyphs():
+    return [
+        '200 OK',
+        [('Content-Type', 'application/json')],
+        [json.dumps([]).encode('utf-8')]
+    ]
 
-    def __call__(self):
-        return self.POST()
+def get_pages():
+    return [
+        '200 OK',
+        [('Content-Type', 'application/json')],
+        [json.dumps([]).encode('utf-8')]
+    ]
 
-class saver:
-    def POST(self):
-        return "Saver not implemented yet."
+def get_documents():
+    return [
+        '200 OK',
+        [('Content-Type', 'application/json')],
+        [json.dumps([]).encode('utf-8')]
+    ]
 
-    def __call__(self):
-        return self.POST()
+# URL routing
+urls = (
+    '/collections', get_collections,
+    '/images', get_images,
+    '/glyphs', get_glyphs,
+    '/pages', get_pages,
+    '/documents', get_documents,
+)
 
-class uploader:
-    def POST(self):
-        return "Uploader not implemented yet."
-
-    def __call__(self):
-        return self.POST()
-
-class adder:
-    def POST(self):
-        return "Adder not implemented yet."
-
-    def __call__(self):
-        return self.POST()
-
-class remover:
-    def POST(self):
-        return "Remover not implemented yet."
-
-    def __call__(self):
-        return self.POST()
-
-# URL mapping
-urls = [
-    ('/', server()),
-    ('/load', loader()),
-    ('/save', saver()),
-    ('/upload', uploader()),
-    ('/add', adder()),
-    ('/remove', remover()),
-]
-
-# Create WSGI app
-# app = application(urls, globals())
+# Build WSGI app
 app = application(urls, globals()).wsgifunc()
