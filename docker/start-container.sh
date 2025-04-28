945b50b7f0c435dcf7c720799736850a189d35cf
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-04-28 17:25:27 +0200

    MYSQL syntax

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -9,9 +9,9 @@ sleep 10
 #echo "CREATE DATABASE glyphminer" | mysql --default-character-set=utf8
 echo CREATE DATABASE IF NOT EXISTS glyphminer | mysql --default-character-set=utf8
 #echo "GRANT USAGE ON *.* TO glyphminer@localhost IDENTIFIED BY 'glyphminer'" | mysql --default-character-set=utf8
-echo CREATE USER IF NOT EXISTS 'glyphminer'@'localhost' IDENTIFIED WITH mysql_native_password BY 'glyphminer' | mysql --default-character-set=utf8
+mysql --default-character-set=utf8 -e "CREATE USER IF NOT EXISTS 'glyphminer'@'localhost' IDENTIFIED WITH mysql_native_password BY 'glyphminer';"
 #echo "GRANT ALL PRIVILEGES ON glyphminer.* TO glyphminer@localhost" | mysql --default-character-set=utf8
-echo GRANT ALL PRIVILEGES ON glyphminer.* TO 'glyphminer'@'localhost' | mysql --default-character-set=utf8
+mysql --default-character-set=utf8 -e "GRANT ALL PRIVILEGES ON glyphminer.* TO 'glyphminer'@'localhost';
 echo FLUSH PRIVILEGES | mysql --default-character-set=utf8
 
 mysql --user=glyphminer --password=glyphminer --default-character-set=utf8 glyphminer < /opt/glyph-miner/server/schema.sql
