be1ebc343f0e4b5da925d84e8397c43697f37ab4
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-04-29 16:46:07 +0200

    logging improved

--- local/run-uwsgi.sh
+++ local/run-uwsgi.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Run the glyph-miner backend using uwsgi
-# This script expects to be run from the project root
+# Create timestamped logfile in current directory
+timestamp=$(date +"%Y%m%d-%H%M%S")
+logfile="uwsgi-$timestamp.log"
 
+echo "Logging to $logfile"
+echo "Ctrl+C to stop."
+
+# Run uwsgi with timestamped log and show print() output
 uwsgi --socket 127.0.0.1:9091 \
       --protocol uwsgi \
       --chdir "$(dirname "$0")/.." \
       --module server.server:app \
       --master --processes 1 --threads 2 \
-      --logto /tmp/uwsgi.log
+      --logto "$logfile"
