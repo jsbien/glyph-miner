83839cba8bcdc6923e861a0a37eddd9e38b75973
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-04-29 13:32:25 +0200

    Add reusable UWSGI runner script to launch server

---
+++ local/run-uwsgi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run the glyph-miner backend using uwsgi
+# This script expects to be run from the project root
+
+uwsgi --socket 127.0.0.1:9091 \
+      --protocol uwsgi \
+      --chdir "$(dirname "$0")/.." \
+      --module server.server:app \
+      --master --processes 1 --threads 2
