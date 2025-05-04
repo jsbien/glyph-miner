f5f5ad22dd1430e50268c4810efb3ae376106732
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-05-04 09:11:56 +0200

    # decode bytes to string first

--- server/server.py
+++ server/server.py
@@ -91,6 +91,9 @@ class collections_handler:
     def POST(self):
         data = webapi.data()
         print(f"[DEBUG] raw data = {repr(data)}", flush=True)
+        # decode bytes to string first
+        if isinstance(data, bytes):
+            data = data.decode("utf-8")
         payload = json.loads(data)
         print(f"[DEBUG] POST /collections - Payload: {payload}", flush=True)
 
