fcc52cb710816fa5679d22b9f0b2f39eae69d9c0
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-05-08 16:32:47 +0200

    Restore transaction context manager in transaction.py after refactoring db module

--- server/webapp/db/transaction.py
+++ server/webapp/db/transaction.py
@@ -1,3 +1,20 @@
+from contextlib import contextmanager
+import server.webapp as web
+
+@contextmanager
+def transaction():
+    """Context manager for database transaction."""
+    db = web.ctx.db
+    db_cursor = db.cursor()
+    try:
+        yield db_cursor
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
+    finally:
+        db_cursor.close()
+
 class MySQLDB:
     def commit(self):
         if self._db:
