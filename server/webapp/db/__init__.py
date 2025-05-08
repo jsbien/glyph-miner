f2d3b39c124c63b1e05e181f32ce21991fa53a7d
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-05-08 18:58:01 +0200

    __init__.py updated

--- server/webapp/db/__init__.py
+++ server/webapp/db/__init__.py
@@ -1,5 +1,7 @@
 from .querying import select, query
 from .transaction import transaction
 from .inserting import insert
-from .inserting import insert
-from .base import database
+from .base import database, register_database
+
+__all__ = ['select', 'transaction', 'insert', 'database', 'register_database']
+
