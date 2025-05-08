746b894952243110a194ac0ed2d27c345556253f
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-05-08 16:41:07 +0200

    Add inserting.py with insert() function

---
+++ server/webapp/db/inserting.py
@@ -0,0 +1,14 @@
+# server/webapp/db/inserting.py
+
+from .base import get_db_cursor
+from .transaction import transaction
+
+def insert(db_cursor, table, **values):
+    keys = ", ".join(values.keys())
+    placeholders = ", ".join(["%s"] * len(values))
+    query = f"INSERT INTO {table} ({keys}) VALUES ({placeholders})"
+    params = list(values.values())
+
+    print(f"[DEBUG] Executing INSERT: {query}")
+    print(f"[DEBUG] With params: {params}")
+    db_cursor.execute(query, params)
