58911038c477c2e1aa8022f05af023a1651fb524
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-05-09 06:43:24 +0200

    Restore MySQLDB abstraction as reusable class in connection.py

--- server/webapp/db/connection.py
+++ server/webapp/db/connection.py
@@ -1,41 +1,64 @@
 # server/webapp/db/connection.py
 
+# server/webapp/db/connection.py
+
 import MySQLdb
+import MySQLdb.cursors
 
 class MySQLDB:
-    """Simple wrapper around MySQLdb for compatibility with legacy code."""
-    def __init__(self, dburl=None, **params):
-        self.connection = MySQLdb.connect(**params)
-        self.cursor = self.connection.cursor()
+    def __init__(self, db=None, user=None, pw=None, host='localhost', port=3306, **kwargs):
+        self.db = db
+        self.user = user
+        self.pw = pw
+        self.host = host
+        self.port = port
+        self.connection = self._connect()
 
-    def query(self, sql, params=None):
-        """Executes a SELECT-like query and returns results."""
-        if params:
-            self.cursor.execute(sql, params)
-        else:
-            self.cursor.execute(sql)
-        return self.cursor.fetchall()
-
-    def execute(self, sql, params=None):
-        """Executes a non-SELECT SQL command (INSERT/UPDATE/DELETE)."""
-        if params:
-            self.cursor.execute(sql, params)
-        else:
-            self.cursor.execute(sql)
-
-    def commit(self):
-        """Commits the current transaction."""
-        self.connection.commit()
-
-    def rollback(self):
-        """Rolls back the current transaction."""
-        self.connection.rollback()
-
-    def close(self):
-        """Closes the DB connection."""
-        self.cursor.close()
-        self.connection.close()
+    def _connect(self):
+        return MySQLdb.connect(
+            db=self.db,
+            user=self.user,
+            passwd=self.pw,
+            host=self.host,
+            port=self.port,
+            cursorclass=MySQLdb.cursors.DictCursor,
+        )
 
     def get_cursor(self):
-        """Returns the raw cursor (legacy compatibility)."""
-        return self.cursor
+        return self.connection.cursor()
+
+    def query(self, sql, params=None):
+        cur = self.get_cursor()
+        cur.execute(sql, params or ())
+        results = cur.fetchall()
+        cur.close()
+        return results
+
+    def insert(self, table, **fields):
+        keys = ', '.join(fields.keys())
+        placeholders = ', '.join(['%s'] * len(fields))
+        sql = f"INSERT INTO {table} ({keys}) VALUES ({placeholders})"
+        params = list(fields.values())
+        cur = self.get_cursor()
+        cur.execute(sql, params)
+        self.connection.commit()
+        cur.close()
+
+    def update(self, table, where, **fields):
+        set_clause = ', '.join([f"{k}=%s" for k in fields])
+        sql = f"UPDATE {table} SET {set_clause} WHERE {where}"
+        params = list(fields.values())
+        cur = self.get_cursor()
+        cur.execute(sql, params)
+        self.connection.commit()
+        cur.close()
+
+    def select(self, table, where=None, params=None):
+        sql = f"SELECT * FROM {table}"
+        if where:
+            sql += f" WHERE {where}"
+        cur = self.get_cursor()
+        cur.execute(sql, params or ())
+        results = cur.fetchall()
+        cur.close()
+        return results
