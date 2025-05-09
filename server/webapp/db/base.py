ec207fd74cb7ff0ba454dd96540de00cc586ef4b
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-05-09 06:24:40 +0200

    Fix database() to support dbn='mysql' argument for compatibility

--- server/webapp/db/base.py
+++ server/webapp/db/base.py
@@ -1,65 +1,19 @@
-import MySQLdb
-import MySQLdb.cursors
+from . import connection
 
-class MySQLDB:
-    def __init__(self, dbn, user, pw, db, host='localhost', port=3306, charset='utf8mb4'):
-        self.dbn = dbn
-        self.user = user
-        self.pw = pw
-        self.db = db
-        self.host = host
-        self.port = port
-        self.charset = charset
-        self._db = None
-        self._cursor = None
-        self.connect()
-
-    def connect(self):
-        if self.dbn != 'mysql':
-            raise ValueError("Only MySQL is currently supported")
-        self._db = MySQLdb.connect(
-            host=self.host,
-            user=self.user,
-            passwd=self.pw,
-            db=self.db,
-            port=self.port,
-            charset=self.charset,
-            use_unicode=True,
-            cursorclass=MySQLdb.cursors.DictCursor
-        )
-        self._cursor = self._db.cursor()
-        print("[DEBUG] MySQLDB connected")
-
-    def close(self):
-        if self._cursor:
-            self._cursor.close()
-        if self._db:
-            self._db.close()
-        print("[DEBUG] MySQLDB connection closed")
-
-    def __del__(self):
-        self.close()
-
-     # In server/webapp/db/base.py
-# import server.webapp.db.connection as conn
-
+# Registry of supported databases
 _databases = {}
 
-from . import connection as conn
-
-def database(dburl=None, **params):
-    return conn.get_connection(dburl, **params)
-
-
-# # Assuming the MySQLDB or equivalent connection class was in connection.py
-# def database(dburl=None, **params):
-#     """ Returns a database connection instance """
-#     if dburl is None:
-#         dburl = "default_database_url"  # You can change this to your default
-#     return conn.MySQLdb(dburl, **params)  # Assuming MySQLDB is in connection.py
-
-# Registering databases (if necessary)
 def register_database(name, clazz):
-    """ Register a database instance by name """
+    """Registers a database backend class under a name like 'mysql'."""
     _databases[name] = clazz
-   
+
+def database(dburl=None, dbn=None, **params):
+    if dbn:
+        if dbn == 'mysql':
+            return MySQLDB(**params)
+        else:
+            raise ValueError(f"Unsupported database type: {dbn}")
+    elif dburl:
+        return MySQLDB(dburl, **params)
+    else:
+        raise ValueError("Either dburl or dbn must be provided.")
