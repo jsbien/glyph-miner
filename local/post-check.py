0757b734e2030ddf2b2cd3ac4fb1804fbff4a5ea
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-05-10 15:35:22 +0200

    add PID to collection name

--- local/post-check.py
+++ local/post-check.py
@@ -1,9 +1,13 @@
 import requests
 import json
 
+with open("/tmp/uwsgi-wrapper.pid") as f:
+    pid = f.read().strip()
+
 url = "http://localhost:9090/api/collections"
 headers = {"Content-Type": "application/json"}
-data = {"title": "Debug Collection"}
+data = {"title": f"Debug Collection {pid}"}
+
 
 response = requests.post(url, headers=headers, data=json.dumps(data))
 
