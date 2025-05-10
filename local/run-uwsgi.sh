74f418dddf7bf8b0399de68c68ca54018b33a78c
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-05-10 12:21:43 +0200

    really revert the last change

--- local/run-uwsgi.sh
+++ local/run-uwsgi.sh
@@ -15,7 +15,7 @@ exec /home/jsbien/git/glyph-miner/uwsgi-env/bin/uwsgi \
   --protocol uwsgi \
   --chdir /home/jsbien/git/glyph-miner \
   --pythonpath /home/jsbien/git/glyph-miner \
-  --module server.server:app  \
+  --module server.server:application \
   --master \
   --processes 1 \
   --threads 2 \
