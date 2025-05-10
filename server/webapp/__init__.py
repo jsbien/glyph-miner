8a4939bb17d60af166ccc74de9a00b200d60bd17
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-05-10 07:39:08 +0200

    Fix import errors in server.webapp.__init__.py by using correct relative imports

--- server/webapp/__init__.py
+++ server/webapp/__init__.py
@@ -8,16 +8,15 @@ print("✅ server.webapp.__init__.py loaded", flush=True)
 from . import utils, net, wsgi, web_http as http, webapi, httpserver, debugerror
 from . import template, form, session, application, browser
 
-from utils import *
-# from db import *
-from net import *
-from wsgi import *
-from web_http import *
-from webapi import *
-from httpserver import *
-from debugerror import *
-from application import *
-from browser import *
+from .utils import *
+from .net import *
+from .wsgi import *
+from .web_http import *
+from .webapi import *
+from .httpserver import *
+from .debugerror import *
+from .application import *
+from .browser import *
 
 try:
     import webopenid as openid
@@ -25,17 +24,3 @@ except ImportError:
     pass  # requires openid module
 
 from server.database import database, select, query, insert, transaction
-
-
-# from .base import database, register_database
-# from .querying import select, query
-# from .inserting import insert
-# from .transaction import transaction
-
-# __all__ = [
-#     "database",
-#     "select",
-#     "query",
-#     "insert",
-#     "transaction"
-# ]
