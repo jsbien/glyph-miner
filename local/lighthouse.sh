2b7b9219e52d7c8fad2674a93acce6225f377c7c
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-05-11 20:47:22 +0200

    lighthouse ... --view

---
+++ local/lighthouse.sh
@@ -0,0 +1,5 @@
+lighthouse http://localhost:9090/#/overview \
+  --only-categories=performance,accessibility,best-practices \
+  --emulated-form-factor=desktop \
+  --output html --output-path ./lighthouse-report.html \
+  --view
