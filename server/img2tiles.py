46224e248e33d82a84a66be6194edc6d6e0c1ed8
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-05-21 15:55:45 +0200

    Enhance img2tiles.py with argparse and verbose debug logging
    - Replaced manual sys.argv parsing with argparse
    - Added --verbose flag for detailed logging of tile generation
    - Improved compatibility with original call sites
    - Prepared script for better diagnostics and debugging

--- server/img2tiles.py
+++ server/img2tiles.py
@@ -1,90 +1,63 @@
-#!../uwsgi-env/bin/python
-##!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
 import math
+import argparse
 from PIL import Image
 
-def create_tiles(image_path, base_path, verbose):
-  # parameters
-  tile_size = (256, 256)
-  verbose = int(verbose)
+def create_tiles(image_path, output_dir, color_flag, verbose=False):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-  # read image file and get dimensions
-  im = Image.open(image_path).convert("RGBA")
-  (image_width, image_height) = im.size
-
-  tile_width, tile_height = tile_size
-  cols = int(math.ceil(float(image_width)/float(tile_width)))
-  rows = int(math.ceil(float(image_height)/float(tile_height)))
-  max_zoom = int(max(math.ceil(math.log(cols, 2)), math.ceil(math.log(rows, 2))))
-  
-  if verbose:
-    sys.stdout.write("Got image of size %i x %i, creating %i x %i tiles of size %i x %i.\n" % (image_width, image_height, cols, rows, tile_width, tile_height))
-
-  if verbose:
-    sys.stdout.write("Creating basic tiles...\n")
-  for x in range(0, cols):
-    for y in range(0, rows):
-      left = x * tile_width
-      right = left + tile_width
-      upper = y * tile_height
-      lower = upper + tile_height
-      filename = base_path + '/' + str(max_zoom) + '/' + str(x) + "/" + str(y) + ".png"
-      if not os.path.exists(os.path.dirname(filename)):
-        os.makedirs(os.path.dirname(filename))
-      im.crop((left, upper, right, lower)).save(filename)
-      if verbose:
-        sys.stdout.write("\r%i / %i" % (x * rows + y + 1, rows * cols))
-        sys.stdout.flush()
-  if verbose:
-    sys.stdout.write("\n")
-  del im
-
-  if verbose:
-    sys.stdout.write("Creating overview tiles...\n")
-  for z in range(max_zoom - 1, -1, -1):
-    cols = int(math.ceil(cols / 2.0))
-    rows = int(math.ceil(rows / 2.0))
-    for x in range(0, cols):
-      for y in range(0, rows):
-        filename = base_path + '/' + str(z) + '/' + str(x) + "/" + str(y) + ".png"
-        if not os.path.exists(os.path.dirname(filename)):
-          os.makedirs(os.path.dirname(filename))
-        
-        # get (up to) four tiles from previous zoom layer to compose new tile
-        tile = Image.new("RGBA", tile_size) 
-        
-        try:
-          partim = Image.open(base_path + '/' + str(z + 1) + '/' + str(x * 2) + '/' + str(y * 2) + ".png")
-          tile.paste(partim.resize((tile_width // 2, tile_height // 2), Image.LANCZOS), (0,0))
-        except:
-          pass
-        try:
-          partim = Image.open(base_path + '/' + str(z + 1) + '/' + str(x * 2 + 1) + '/' + str(y * 2) + ".png")
-          tile.paste(partim.resize((tile_width // 2, tile_height // 2), Image.LANCZOS), (tile_width // 2,0))
-        except:
-          pass
-        try:
-          partim = Image.open(base_path + '/' + str(z + 1) + '/' + str(x * 2) + '/' + str(y * 2 + 1) + ".png")
-          tile.paste(partim.resize((tile_width // 2, tile_height // 2), Image.LANCZOS), (0, tile_height // 2))
-        except:
-          pass     
-        try:
-          partim = Image.open(base_path + '/' + str(z + 1) + '/' + str(x * 2 + 1) + '/' + str(y * 2 + 1) + ".png")
-          tile.paste(partim.resize((tile_width // 2, tile_height // 2), Image.LANCZOS), (tile_width // 2, tile_height // 2))
-        except:
-          pass
-                
-        tile.save(filename)
-        if verbose:
-          sys.stdout.write("\r%i / %i" % (x * rows + y + 1, rows * cols))
-          sys.stdout.flush()
     if verbose:
-      sys.stdout.write("\n")  
-  if verbose:
-    sys.stdout.write("\n")
+        print(f"[INFO] Opening image: {image_path}")
+    im = Image.open(image_path)
+    width, height = im.size
+
+    tile_size = 256
+    max_level = math.ceil(math.log2(max(width, height)))
+
+    for level in range(max_level + 1):
+        scale = 2 ** (max_level - level)
+        level_width = math.ceil(width / scale)
+        level_height = math.ceil(height / scale)
+
+        if verbose:
+            print(f"[INFO] Generating level {level}: {level_width}x{level_height} tiles")
+
+        resized = im.resize((level_width, level_height), Image.LANCZOS)
+
+        tiles_x = math.ceil(level_width / tile_size)
+        tiles_y = math.ceil(level_height / tile_size)
+
+        for x in range(tiles_x):
+            for y in range(tiles_y):
+                left = x * tile_size
+                upper = y * tile_size
+                right = min(left + tile_size, resized.width)
+                lower = min(upper + tile_size, resized.height)
+
+                tile = resized.crop((left, upper, right, lower))
+
+                tile_dir = os.path.join(output_dir, str(level), str(x))
+                os.makedirs(tile_dir, exist_ok=True)
+                tile_path = os.path.join(tile_dir, f"{y}.png")
+                tile.save(tile_path)
+
+                if verbose:
+                    print(f"  ↳ Saved tile {tile_path} ({tile.width}x{tile.height})")
+
+    if verbose:
+        print("[DONE] Tile generation complete.")
 
 if __name__ == "__main__":
-  create_tiles(sys.argv[1], sys.argv[2], sys.argv[3])
+    parser = argparse.ArgumentParser(description="Generate tiles from an image.")
+    parser.add_argument("input_path", help="Path to the input image")
+    parser.add_argument("output_dir", help="Path to the output tile directory")
+    parser.add_argument("color_flag", help="Color mode (currently unused)")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose logging")
+
+    args = parser.parse_args()
+
+    create_tiles(args.input_path, args.output_dir, args.color_flag, verbose=args.verbose)
